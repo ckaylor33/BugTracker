a079ae6d62c470469921a1227eb4f1ff42839177
Charlie Kaylor <68200523+ckaylor33@users.noreply.github.com>
2022-07-29 09:43:04 +0100

    Project details view now functional - style could be better so will return to this. Data passed to the view that I need, had to adapt project service and details action in project controller to do this.

--- Services/BTProjectService.cs
+++ Services/BTProjectService.cs
@@ -234,13 +234,30 @@ namespace BugTracker.Services
         #region GetProjectByIdAsync
         public async Task<Project> GetProjectByIdAsync(int projectId, int companyId)
         {
-            Project project = await _context.Projects
-                                            .Include(p => p.Tickets)
-                                            .Include(p => p.Members)
-                                            .Include(p => p.ProjectPriority)
-                                            .FirstOrDefaultAsync(p => p.Id == projectId && p.CompanyId == companyId);
+            try
+            {
+                Project project = await _context.Projects
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.TicketPriority)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.TicketStatus)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.TicketType)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.DeveloperUser)
+                                                .Include(p => p.Tickets)
+                                                    .ThenInclude(t => t.OwnerUser)
+                                                .Include(p => p.Members)
+                                                .Include(p => p.ProjectPriority)
+                                                .FirstOrDefaultAsync(p => p.Id == projectId && p.CompanyId == companyId);
+                return project;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
 
-            return project;
+
         }
         #endregion
 
